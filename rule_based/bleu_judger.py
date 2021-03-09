cb1256c26bb4dd737653f7bed41cffc22e6a1c48
Duy-Bach Le <duybach.224575@gmail.com>
2021-03-09 10:55:24 +0000

    Minor bugfix

--- rule_based/bleu_judger.py
+++ rule_based/bleu_judger.py
@@ -1,5 +1,6 @@
 from nltk.translate.bleu_score import corpus_bleu, sentence_bleu
 from sys import argv
+from nltk import word_tokenize
 
 true_output_file = open("../analyzing_development/output.txt", "r")
 answer_file = open(argv[1], "r")
@@ -11,11 +12,11 @@ while True:
 	out, ans = true_output_file.readline(), answer_file.readline()
 	if out == '' or ans == '': break
 
-	print(str(out.split()) + "\n" + str(ans.split()) + "\n")
-	print('sentence BLEU =', sentence_bleu([out.split()], ans.split()), "\n")
+	print(str(out.split()) + "\n" + str(word_tokenize(ans)) + "\n")
+	print('sentence BLEU =', sentence_bleu([word_tokenize(out)], word_tokenize(ans)), "\n")
 
-	true_outputs.append([out.split()])
-	answers.append(ans.split())
+	true_outputs.append([word_tokenize(out)])
+	answers.append(word_tokenize(ans))
 
 	# prompt = input()
 
