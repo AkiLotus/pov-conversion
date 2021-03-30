0a08bf4a6a633e2753eee0d4e8cfa925bde99ed1
Duy-Bach Le <duybach.224575@gmail.com>
2021-03-30 10:55:40 +0000

    Edit data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -1,4 +1,5 @@
 import re
+import numpy as np
 
 class Utilities:
 	def print_row(row, spacing=32):
@@ -108,6 +109,71 @@ class TSVCorpus:
 		list_output = []
 		for key in self.headers: list_output.append(self.rows[index][key])
 		return list_output
+
+	def __str__(self):
+		Utilities.print_row([""] + self.headers)
+		for index in range(len(self.rows)):
+			itemlist = [index]
+			for key in self.headers: itemlist.append(self.rows[index][key])
+			Utilities.print_row(itemlist)
+		return ''
+
+class TSVFile:
+	def __init__(self, file_path = "", has_headers = True):
+
+		self.headers = []
+		self.rows = []
+
+		if file_path:
+			file = open(file_path, "r")
+
+			if has_headers:
+				self.headers = file.readline().strip().split('\t')
+			
+			while True:
+				try:
+					row = file.readline().strip().split('\t')
+					if len("".join(row)) == 0: break
+
+					if len(self.headers) == 0:
+						self.headers = [x for x in range(len(row))]
+					
+					dicted_row = {}
+					for index in range(len(row)):
+						dicted_row[self.headers[index]] = row[index]
+					
+					self.rows.append(dicted_row)
+				except EOFError: break
+	
+	def get_row(self, index, as_list=False):
+		if not as_list: return self.rows[index]
+
+		list_output = []
+		for key in self.headers: list_output.append(self.rows[index][key])
+		return list_output
+	
+	def get_column(self, string, as_list=False):
+		list_output = []
+
+		for index in range(len(self.rows)):
+			list_output.append(self.rows[index][string])
+
+		return list_output
+	
+	def random_split(self, p1_proportion = 0.5, seed = 170299):
+		length = len(self.rows)
+		np.random.seed(seed)
+		permutation = np.random.permutation(length) + 1
+
+		split_1, split_2 = TSVFile(), TSVFile()
+		split_1.headers = [x for x in self.headers]
+		split_2.headers = [x for x in self.headers]
+		for index in range(length):
+			if permutation[index] <= p1_proportion * length:
+				split_1.rows.append(self.rows[index])
+			else:
+				split_2.rows.append(self.rows[index])
+		return split_1, split_2
 
 	def __str__(self):
 		Utilities.print_row([""] + self.headers)
